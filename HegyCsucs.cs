d6b0933f8796158f8db361cd4d527ef8b73eba38
monusbence <72020098+monusbence@users.noreply.github.com>
2023-05-18 14:20:41 +0200

    Add files via upload

---
+++ HegyCsucs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HegyekMo2
+{
+    internal class HegyCsucs
+    {
+        string nev;
+        string hegyseg;
+        int magassag;
+
+        public HegyCsucs(string nev, string hegyseg, int magassag)
+        {
+            this.nev = nev;
+            this.hegyseg = hegyseg;
+            this.magassag = magassag;
+        }
+
+        public string Nev { get => nev;}
+        public string Hegyseg { get => hegyseg;}
+        public int Magassag { get => magassag;}
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Configuration;
+using System.IO;
+namespace HegyekMo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] szoveg = File.ReadAllLines("C:\\Users\\Mónus Bence\\source\\repos\\HegyekMo2\\HegyekMo2\\hegyekMo.txt");
+            List<HegyCsucs> csucsok = new List<HegyCsucs>();
+
+            foreach (var hegy in szoveg.Skip(1)) {
+                string[] adatok = hegy.Split(';');
+                HegyCsucs ujHegyCsucs = new HegyCsucs(adatok[0], adatok[1], Convert.ToInt32(adatok[2]));
+                csucsok.Add(ujHegyCsucs);
+            }
+            //3. feladat:
+            Console.WriteLine($"3. feladat: Hegycsúcsok száma: {csucsok.Count} db");
+            //4. feladat:
+            Console.WriteLine($"4. feladat: Hegycsúcsok átlagos magassága: {csucsok.Average(x => x.Magassag)} m");
+            //5.feladat:
+            Console.WriteLine($"5. feladat: {csucsok.Max(x => x.Magassag)} m");
+        }
+    }
+}
